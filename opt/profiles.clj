707b75bf920a84d84777076318ed9fa4a2431d6b
Phil Hagelberg <technomancy@gmail.com>
2012-08-09 17:28:41 -0700

    Mirrors in production and user profiles.

--- opt/profiles.clj
+++ opt/profiles.clj
@@ -1,2 +1,5 @@
-{:user {:plugins [[lein-clean-m2 "0.1.1"]]
-        :mirrors {#"central|clojars" "http://s3pository.herokuapp.com/clojure"}}}
+{:user {:plugins [[lein-clean-m2 "0.1.2"]]
+        :mirrors {#"central|clojars"
+                  "http://s3pository.herokuapp.com/clojure"}}
+ :production {:mirrors {#"central|clojars"
+                        "http://s3pository.herokuapp.com/clojure"}}}
