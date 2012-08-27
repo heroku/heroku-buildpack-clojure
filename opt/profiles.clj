e1c41a21e4eb4e68d37c0f4c9ca9183376c9f699
Phil Hagelberg <technomancy@gmail.com>
2012-08-27 15:00:17 -0700

    Quit running clean-m2.
    This was resulting in resolving plugins at dyno launch time.

--- opt/profiles.clj
+++ opt/profiles.clj
@@ -1,5 +1,4 @@
-{:user {:plugins [[lein-clean-m2 "0.1.2"]]
-        :mirrors {#"central|clojars"
+{:user {:mirrors {#"central|clojars"
                   "http://s3pository.herokuapp.com/clojure"}}
  :production {:mirrors {#"central|clojars"
                         "http://s3pository.herokuapp.com/clojure"}}}
