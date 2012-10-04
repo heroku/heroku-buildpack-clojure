418735938ddaf8d4793a239f5d35958ab160ae5a
Phil Hagelberg <technomancy@gmail.com>
2012-10-04 14:44:16 -0700

    S3pository for clojars only.

--- opt/profiles.clj
+++ opt/profiles.clj
@@ -1,8 +1,5 @@
-{:user {:mirrors {#"central|clojars"
-                  "http://s3pository.herokuapp.com/clojure"}
+{:user {:mirrors {"clojars" "http://s3pository.herokuapp.com/clojure"}
         :aliases {"repl" "standalone-repl"}
         :plugins [[lein-standalone-repl "0.1.3"]]}
- :production {:mirrors {#"central|clojars"
-                        "http://s3pository.herokuapp.com/clojure"}}
- :heroku {:mirrors {#"central|clojars"
-                    "http://s3pository.herokuapp.com/clojure"}}}
+ :production {:mirrors {"clojars" "http://s3pository.herokuapp.com/clojure"}}
+ :heroku {:mirrors {"clojars" "http://s3pository.herokuapp.com/clojure"}}}
