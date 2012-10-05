0ae6a4ede193a3053a639bd081543ad60285a260
Phil Hagelberg <technomancy@gmail.com>
2012-10-04 17:01:42 -0700

    Separate mirrors for central vs clojars.

--- opt/profiles.clj
+++ opt/profiles.clj
@@ -1,5 +1,8 @@
-{:user {:mirrors {"clojars" "http://s3pository.herokuapp.com/clojure"}
+{:user {:mirrors {"central" "http://s3pository.herokuapp.com/maven-central"
+                  "clojars" "http://s3pository.herokuapp.com/clojars"}
         :aliases {"repl" "standalone-repl"}
         :plugins [[lein-standalone-repl "0.1.3"]]}
- :production {:mirrors {"clojars" "http://s3pository.herokuapp.com/clojure"}}
- :heroku {:mirrors {"clojars" "http://s3pository.herokuapp.com/clojure"}}}
+ :production {:mirrors {"central" "http://s3pository.herokuapp.com/maven-central"
+                        "clojars" "http://s3pository.herokuapp.com/clojars"}}
+ :heroku {:mirrors {"central" "http://s3pository.herokuapp.com/maven-central"
+                    "clojars" "http://s3pository.herokuapp.com/clojars"}}}
