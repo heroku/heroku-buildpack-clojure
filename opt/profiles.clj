1c210356ef28fea986b166880246a5e5c02dac39
Phil Hagelberg <technomancy@gmail.com>
2013-01-23 11:44:12 -0800

    Remove backported repl trampoline bugfix. Add :offline to production profile.

--- opt/profiles.clj
+++ opt/profiles.clj
@@ -2,11 +2,11 @@
                   ;; TODO: re-enable once clojars releases repo is up
                   ;; "clojars" "http://s3pository.herokuapp.com/clojars"
                   }
-        :aliases {"repl" "standalone-repl"}
-        :plugins [[lein-standalone-repl "0.1.3"]]}
+        :aliases {"repl" ["trampoline" "repl"]}}
  :production {:mirrors {"central" "http://s3pository.herokuapp.com/maven-central"
                         ;; "clojars" "http://s3pository.herokuapp.com/clojars"
-                        }}
+                        }
+              :offline true}
  :heroku {:mirrors {"central" "http://s3pository.herokuapp.com/maven-central"
                     ;; "clojars" "http://s3pository.herokuapp.com/clojars"
                     }}}
