2ebf0ee5ca6ce4007cd6d0a081d82828f23bcda8
Phil Hagelberg <technomancy@gmail.com>
2013-01-25 09:36:16 -0800

    Still need standalone-repl plugin for non-containerized network.

--- opt/profiles.clj
+++ opt/profiles.clj
@@ -2,7 +2,8 @@
                   ;; TODO: re-enable once clojars releases repo is up
                   ;; "clojars" "http://s3pository.herokuapp.com/clojars"
                   }
-        :aliases {"repl" ["trampoline" "repl"]}}
+        :aliases {"repl" "standalone-repl"}
+        :plugins [[lein-standalone-repl "0.1.4"]]}}
  :production {:mirrors {"central" "http://s3pository.herokuapp.com/maven-central"
                         ;; "clojars" "http://s3pository.herokuapp.com/clojars"
                         }
