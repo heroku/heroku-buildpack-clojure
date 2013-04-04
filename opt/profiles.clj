82c09ae91d6ac86a7dff8955221be13532b50c98
Phil Hagelberg <technomancy@gmail.com>
2013-04-04 11:49:37 -0700

    Back to standalone-repl plugin until 2.1.3.

--- opt/profiles.clj
+++ opt/profiles.clj
@@ -2,9 +2,8 @@
                   ;; TODO: re-enable once clojars releases repo is up
                   ;; "clojars" "http://s3pository.herokuapp.com/clojars"
                   }
-        :aliases {"repl" ["update-in" ":dependencies" "conj" "[reply \"0.1.6\"]" "--"
-                          "with-profile" "production" "trampoline" "run" "-m"
-                          "reply.main/launch-standalone" "nil"]}}
+        :aliases {"repl" "standalone-repl"}
+        :plugins [[lein-standalone-repl "0.1.5"]]}
  :production {:mirrors {"central" "http://s3pository.herokuapp.com/maven-central"
                         ;; "clojars" "http://s3pository.herokuapp.com/clojars"
                         }
