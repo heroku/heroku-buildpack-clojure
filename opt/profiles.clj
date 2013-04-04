51973eb28fffe2e6168e14048fbb35ac33a069b1
Phil Hagelberg <technomancy@gmail.com>
2013-04-04 11:40:51 -0700

    Use the fancy reply repl instead of raw trampoline.

--- opt/profiles.clj
+++ opt/profiles.clj
@@ -2,7 +2,9 @@
                   ;; TODO: re-enable once clojars releases repo is up
                   ;; "clojars" "http://s3pository.herokuapp.com/clojars"
                   }
-        :aliases {"repl" ["with-profile" "production" "trampoline" "repl"]}}
+        :aliases {"repl" ["update-in" ":dependencies" "conj" "[reply \"0.1.6\"]" "--"
+                          "with-profile" "production" "trampoline" "run" "-m"
+                          "reply.main/launch-standalone" "nil"]}}
  :production {:mirrors {"central" "http://s3pository.herokuapp.com/maven-central"
                         ;; "clojars" "http://s3pository.herokuapp.com/clojars"
                         }
