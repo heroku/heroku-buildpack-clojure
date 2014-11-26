881964d733673abd6bc4761442fd4c2a09043b7c
Joe Kutner <jpkutner@gmail.com>
2014-11-25 21:41:39 -0600

    added some lein2 tests

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -2,7 +2,7 @@
 
 . ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
 
-_createProject() {
+_createNoLeinProject() {
   cat > ${BUILD_DIR}/project.clj <<EOF
 (defproject sample "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
@@ -26,8 +26,23 @@ EOF
 EOF
 }
 
+_createLein2ProjectFile() {
+  cat > ${BUILD_DIR}/project.clj <<EOF
+  (defproject sample "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+  :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :min-lein-version "2.0.0"
+  :main ^:skip-aot sample.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
+EOF
+}
+
 _createSysProps() {
-  local jdkVersion=${1:-"1.6"}
+  local jdkVersion=${1:-"1.8"}
   cat > ${BUILD_DIR}/system.properties <<EOF
 java.runtime.version=${jdkVersion}
 EOF
@@ -36,7 +51,7 @@ EOF
 # Tests
 
 testCompileJdk6() {
-  _createProject
+  _createNoLeinProject
   _createSysProps "1.6"
   compile
   assertCapturedSuccess
@@ -45,7 +60,7 @@ testCompileJdk6() {
 }
 
 testCompileJdk7() {
-  _createProject
+  _createNoLeinProject
   _createSysProps "1.7"
   compile
   assertCapturedSuccess
@@ -54,9 +69,20 @@ testCompileJdk7() {
 }
 
 testCompileJdk8() {
-  _createProject
+  _createNoLeinProject
   compile
   assertCapturedSuccess
   assertCaptured "Installing OpenJDK 1.8...done"
+  assertCaptured "WARNING: no :min-lein-version found in project.clj; using 1.7.1."
+  assertCaptured "To use Leiningen 2.x, add this to project.clj: :min-lein-version \"2.0.0\""
   assertCaptured "Downloading: leiningen-1.7.1-standalone.jar"
+}
+
+testMinLeinVersion() {
+  _createNoLeinProject
+  _createLein2ProjectFile
+  compile
+  assertCapturedSuccess
+  assertNotCaptured "WARNING: no :min-lein-version found in project.clj; using 1.7.1."
+  assertCaptured "Downloading: leiningen-2.4.2-standalone.jar"
 }
