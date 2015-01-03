84a833a98250c892127a50f79ed320f0390a074b
Joe Kutner <jpkutner@gmail.com>
2015-01-03 10:46:46 -0600

    Cleaned up the publish script so that the clojure term is only used in one place. Makes script more portable

--- etc/publish.sh
+++ etc/publish.sh
@@ -2,25 +2,27 @@
 
 set -e
 
+BP_NAME="clojure"
+
 if [ ! -z "$1" ]; then
   pushd . > /dev/null 2>&1
   cd /tmp &&
-  git clone git@github.com:heroku/heroku-buildpack-clojure.git
-  cd heroku-buildpack-clojure
+  git clone git@github.com:heroku/heroku-buildpack-$BP_NAME.git
+  cd heroku-buildpack-$BP_NAME
   git checkout master
   find . ! -name '.' ! -name '..' ! -name 'bin' ! -name 'opt' -maxdepth 1 -print0 | xargs -0 rm -rf --
-  heroku buildpacks:publish $1/clojure
+  heroku buildpacks:publish $1/$BP_NAME
 
   if [ "$1" = "heroku" ]; then
     echo "Tagging commit... "
-    newTag=$(heroku buildpacks:revisions heroku/clojure | sed -n 2p | grep -o -e "v\d*")
+    newTag=$(heroku buildpacks:revisions heroku/$BP_NAME | sed -n 2p | grep -o -e "v\d*")
     git tag $newTag
     git push --tags
   fi
 
   popd > /dev/null 2>&1
   echo "Cleaning up..."
-  rm -rf /tmp/heroku-buildpack-clojure
+  rm -rf /tmp/heroku-buildpack-$BP_NAME
   echo "Done."
 else
   echo "You must provide a buildkit organization as an argument!"
