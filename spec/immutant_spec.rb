9ae8aba04d4c2a7943a2a9dd1232d586b3fa4ad3
Joe Kutner <jpkutner@gmail.com>
2015-01-12 16:36:12 -0600

    Added basic hatchet tests

---
+++ spec/immutant_spec.rb
@@ -0,0 +1,24 @@
+require_relative 'spec_helper'
+
+describe "Immutant" do
+  before(:each) do
+    set_java_version(app.directory, jdk_version)
+  end
+
+  %w{1.8}.each do |version|
+    context "on jdk-#{version}" do
+      let(:app) { Hatchet::Runner.new("immutant-feature-demo") }
+      let(:jdk_version) { version }
+      it "deploy successfully" do
+        app.deploy do |app|
+          expect(app.output).to include("Installing OpenJDK #{jdk_version}")
+          expect(app.output).to include("Downloading: leiningen-2.5.1-standalone.jar")
+          expect(app.output).to include("Running: lein uberjar")
+          expect(app.output).to include("Compiling demo.core")
+          expect(app).to be_deployed
+          expect(successful_body(app, path: "index.html")).to include("Feature Demo")
+        end
+      end
+    end
+  end
+end
