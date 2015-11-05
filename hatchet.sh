acef69609ed4369455784661c5c1ca18eba598fc
Joe Kutner <jpkutner@gmail.com>
2015-11-05 08:45:10 -0600

    Add hatchet tests to travis config

---
+++ hatchet.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+gem install heroku_hatchet
+gem install rspec-retry
+gem install rspec-expectations
+
+hatchet install &&
+HATCHET_RETRIES=3 \
+HATCHET_DEPLOY_STRATEGY=git \
+HATCHET_BUILDPACK_BASE="https://github.com/heroku/heroku-buildpack-clojure.git" \
+HATCHET_BUILDPACK_BRANCH=$(git name-rev HEAD 2> /dev/null | sed 's#HEAD\ \(.*\)#\1#') \
+rspec spec/
