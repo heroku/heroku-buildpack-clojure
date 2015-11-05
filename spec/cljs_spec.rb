a529fe0849666082d0d7118f897d008d4d50e162
Joe Kutner <jpkutner@gmail.com>
2015-11-05 09:27:38 -0600

    New cljs specs

--- spec/cljs_spec.rb
+++ spec/cljs_spec.rb
@@ -21,4 +21,21 @@ describe "ClojureScript" do
       end
     end
   end
+
+  %w{1.8}.each do |version|
+    context "on jdk-#{version}" do
+      let(:app) { Hatchet::Runner.new("cljs-lein-npm-example") }
+      let(:jdk_version) { version }
+      it "deploy successfully" do
+        app.deploy do |app|
+          expect(app.output).to include("Installing OpenJDK #{jdk_version}")
+          expect(app.output).to match(/Downloading: leiningen-2.5.[0-9]-standalone.jar/)
+          expect(app.output).to include("Running: lein with-profile production do deps, compile :all")
+          sleep 3
+          expect(app).to be_deployed
+          expect(successful_body(app)).to include('Jokes')
+        end
+      end
+    end
+  end
 end
