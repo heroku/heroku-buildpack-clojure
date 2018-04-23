102d20a0c866b1362bdc7ba7b7ceec0d5aa79332
Joe Kutner <jpkutner@gmail.com>
2018-04-23 13:03:48 -0500

    Better publish script

--- etc/publish.sh
+++ etc/publish.sh
@@ -2,24 +2,15 @@
 
 set -e
 
-BP_NAMESPACE="heroku"
-BP_NAME="clojure"
-
 BP_VERSION=${1:?}
+BP_NAME=${2:-"heroku/clojure"}
 
-pushd . > /dev/null 2>&1
-cd /tmp
-rm -rf heroku-buildpack-$BP_NAME
-git clone git@github.com:heroku/heroku-buildpack-$BP_NAME.git
-cd heroku-buildpack-$BP_NAME
-git checkout master
-
-headHash=$(git rev-parse HEAD)
-echo "Tagging commit $headHash with $BP_VERSION... "
-git tag $BP_VERSION
+originMaster=$(git rev-parse origin/master)
+echo "Tagging commit $originMaster with $BP_VERSION... "
+git tag $BP_VERSION ${originMaster:?}
 git push --tags
 
-heroku buildpacks:publish $BP_NAMESPACE/$BP_NAME $BP_VERSION
+heroku buildpacks:publish $BP_NAME $BP_VERSION
 
 echo "Updating previous-version tag"
 git tag -d previous-version
@@ -28,7 +19,7 @@ git tag previous-version latest-version
 echo "Updating latest-version tag"
 git tag -d latest-version
 git push origin :latest-version
-git tag latest-version
+git tag latest-version ${originMaster:?}
 git push --tags
 
 echo "Done."
