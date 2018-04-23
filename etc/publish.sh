97effb5c686cc26428bc79ca909b4ec97f4e6b67
Joe Kutner <jpkutner@gmail.com>
2018-04-23 12:05:08 -0500

    Added buildpack descriptor and new publish script

--- etc/publish.sh
+++ etc/publish.sh
@@ -2,47 +2,33 @@
 
 set -e
 
+BP_NAMESPACE="heroku"
 BP_NAME="clojure"
 
-if [ ! -z "$1" ]; then
-  pushd . > /dev/null 2>&1
-  cd /tmp
-  rm -rf heroku-buildpack-$BP_NAME
-  git clone git@github.com:heroku/heroku-buildpack-$BP_NAME.git
-  cd heroku-buildpack-$BP_NAME
-  git checkout master
-  headHash=$(git rev-parse HEAD)
+BP_VERSION=${1:?}
 
-  find . ! -name '.' ! -name '..' ! -name 'bin' ! -name 'opt' \
-         ! -name 'lib' -maxdepth 1 -print0 | xargs -0 rm -rf --
-  heroku buildkits:publish $1/$BP_NAME
+pushd . > /dev/null 2>&1
+cd /tmp
+rm -rf heroku-buildpack-$BP_NAME
+git clone git@github.com:heroku/heroku-buildpack-$BP_NAME.git
+cd heroku-buildpack-$BP_NAME
+git checkout master
 
-  if [ "$1" = "heroku" ]; then
-    newTag=$(heroku buildkits:revisions heroku/$BP_NAME | sed -n 2p | grep -o -e "v\d*")
-  fi
+headHash=$(git rev-parse HEAD)
+echo "Tagging commit $headHash with $BP_VERSION... "
+git tag $BP_VERSION
+git push --tags
 
-  popd > /dev/null 2>&1
-  echo "Cleaning up..."
-  rm -rf /tmp/heroku-buildpack-$BP_NAME
+heroku buildpacks:publish $BP_NAMESPACE/$BP_NAME $BP_VERSION
 
-  if [ "$1" = "heroku" ]; then
-    if [ "$headHash" = "$(git rev-parse HEAD)" ]; then
-      echo "Tagging commit $headHash with $newTag... "
-      git tag $newTag
-      echo "Updating previous-version tag"
-      git tag -d previous-version
-      git push origin :previous-version
-      git tag previous-version latest-version
-      echo "Updating latest-version tag"
-      git tag -d latest-version
-      git push origin :latest-version
-      git tag latest-version
-      git push --tags
-    fi
-  fi
+echo "Updating previous-version tag"
+git tag -d previous-version
+git push origin :previous-version
+git tag previous-version latest-version
+echo "Updating latest-version tag"
+git tag -d latest-version
+git push origin :latest-version
+git tag latest-version
+git push --tags
 
-  echo "Done."
-else
-  echo "You must provide a buildkit organization as an argument!"
-  exit 1
-fi
+echo "Done."
