458a6bb603a7742b92bd83c922e4a7c46dd6bef1
schneems <richard.schneeman+foo@gmail.com>
2020-08-11 13:27:11 -0500

    [changelog skip] Hatchet v7.0.0
    - ActiveSupport's Object#blank? and Object#present? are no longer provided by default (https://github.com/heroku/hatchet/pull/107)
    - Remove deprecated support for passing a block to `App#run` (https://github.com/heroku/hatchet/pull/105)
    - Ignore  403 on app delete due to race condition (https://github.com/heroku/hatchet/pull/101)
    - The hatchet.lock file can now be locked to "main" in addition to "master" (https://github.com/heroku/hatchet/pull/86)
    - Allow concurrent one-off dyno runs with the `run_multi: true` flag on apps (https://github.com/heroku/hatchet/pull/94)
    - Apps are now marked as being "finished" by enabling maintenance mode on them when `teardown!` is called. Finished apps can be reaped immediately (https://github.com/heroku/hatchet/pull/97)
    - Applications that are not marked as "finished" will be allowed to live for a HATCHET_ALIVE_TTL_MINUTES duration before they're deleted by the reaper to protect against deleting an app mid-deploy, default is seven minutes (https://github.com/heroku/hatchet/pull/97)
    - The HEROKU_APP_LIMIT env var no longer does anything, instead hatchet application reaping is manually executed if an app cannot be created (https://github.com/heroku/hatchet/pull/97)
    - App#deploy without a block will no longer run `teardown!` automatically (https://github.com/heroku/hatchet/pull/97)
    - Calls to `git push heroku` are now rate throttled (https://github.com/heroku/hatchet/pull/98)
    - Calls to `app.run` are now rate throttled (https://github.com/heroku/hatchet/pull/99)
    - Deployment now raises and error when the release failed (https://github.com/heroku/hatchet/pull/93)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -53,10 +53,3 @@ def set_java_version(d, v)
     `git commit -am "setting jdk version"`
   end
 end
-
-ReplRunner.register_commands(:console)  do |config|
-  config.terminate_command "exit"          # the command you use to end the 'rails console'
-  config.startup_timeout 60                # seconds to boot
-  config.return_char "\n"                  # the character that submits the command
-  config.sync_stdout "STDOUT.sync = true"  # force REPL to not buffer standard out
-end
