5717c8c554e6176d807647595193a8a02fc51b1e
Manuel Fuchs <manuel.fuchs@salesforce.com>
2025-11-24 18:46:32 +0100

    Fix rubocop linting errors

--- test/spec/compojure_spec.rb
+++ test/spec/compojure_spec.rb
@@ -1,17 +1,19 @@
-require_relative "spec_helper"
+# frozen_string_literal: true
 
-describe "Compojure" do
-  context "on jdk-#{DEFAULT_OPENJDK_VERSION}" do
-    it "deploys clojure-minimal successfully" do
-      new_default_hatchet_runner("clojure-minimal").tap do |app|
+require_relative 'spec_helper'
+
+describe 'Compojure' do
+  context "when using jdk-#{DEFAULT_OPENJDK_VERSION}" do
+    it 'deploys clojure-minimal successfully' do
+      new_default_hatchet_runner('clojure-minimal').tap do |app|
         app.before_deploy do
-          set_java_version(DEFAULT_OPENJDK_VERSION)
+          java_version(DEFAULT_OPENJDK_VERSION)
         end
 
         app.deploy do
           expect(app.output).to match(/Installing (Heroku|Azul Zulu) OpenJDK #{DEFAULT_OPENJDK_VERSION}/)
           expect(app.output).to match(/Downloading: leiningen-2.[0-9].[0-9]-standalone.jar/)
-          expect(app.output).to include("Running: lein uberjar")
+          expect(app.output).to include('Running: lein uberjar')
           expect(http_get(app)).to include('["Hello" :from Heroku]')
         end
       end
