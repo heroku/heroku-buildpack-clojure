77f57c1397d31d7c4fb81742b5cfd93ba89f07ee
Manuel Fuchs <manuel.fuchs@salesforce.com>
2025-11-25 13:23:14 +0100

    Apply JVM buildpack code style (#181)

--- lib/common.sh
+++ lib/common.sh
@@ -1,54 +1,54 @@
 #!/usr/bin/env bash
 
-cache_copy() {
-	rel_dir=$1
-	from_dir=$2
-	to_dir=$3
-	rm -rf "${to_dir:?}/$rel_dir"
-	if [ -d "$from_dir/$rel_dir" ]; then
-		mkdir -p "$to_dir/$rel_dir"
-		cp -pr "$from_dir/$rel_dir/." "$to_dir/$rel_dir"
+function cache_copy() {
+	local rel_dir="${1}"
+	local from_dir="${2}"
+	local to_dir="${3}"
+	rm -rf "${to_dir:?}/${rel_dir}"
+	if [[ -d "${from_dir}/${rel_dir}" ]]; then
+		mkdir -p "${to_dir}/${rel_dir}"
+		cp -pr "${from_dir}/${rel_dir}/." "${to_dir}/${rel_dir}"
 	fi
 }
 
 # Install node.js
-install_nodejs() {
+function install_nodejs() {
 	local version="${1:?}"
 	local dir="${2:?}"
 	local url="https://heroku-nodebin.s3.us-east-1.amazonaws.com/node/release/linux-x64/node-v${version}-linux-x64.tar.gz"
 
-	echo "Downloading Node.js $version..."
+	echo "Downloading Node.js ${version}..."
 	local code
-	code=$(curl "$url" -L --silent --fail --retry 5 --retry-max-time 15 --retry-connrefused --connect-timeout 5 -o /tmp/node.tar.gz --write-out "%{http_code}")
-	if [ "$code" != "200" ]; then
-		echo "Unable to download Node.js version ${version}: $code"
+	code=$(curl "${url}" -L --silent --fail --retry 5 --retry-max-time 15 --retry-connrefused --connect-timeout 5 -o /tmp/node.tar.gz --write-out "%{http_code}")
+	if [[ "${code}" != "200" ]]; then
+		echo "Unable to download Node.js version ${version}: ${code}"
 		return 1
 	fi
 
 	tar xzf /tmp/node.tar.gz -C /tmp
-	mv "/tmp/node-v$version-linux-x64" "$dir"
-	chmod +x "$dir/bin/"*
+	mv "/tmp/node-v${version}-linux-x64" "${dir}"
+	chmod +x "${dir}/bin/"*
 }
 
-detect_and_install_nodejs() {
-	local buildDir=${1}
-	if [ ! -d "${buildDir}/.heroku/nodejs" ] && [ "true" != "$SKIP_NODEJS_INSTALL" ]; then
-		if [ "$(grep lein-npm "${buildDir}/project.clj")" != "" ] || [ -n "$NODEJS_VERSION" ]; then
-			nodejsVersion=${NODEJS_VERSION:-18.16.0}
+function detect_and_install_nodejs() {
+	local buildDir="${1}"
+	if [[ ! -d "${buildDir}/.heroku/nodejs" ]] && [[ "true" != "${SKIP_NODEJS_INSTALL}" ]]; then
+		if grep -q lein-npm "${buildDir}/project.clj" || [[ -n "${NODEJS_VERSION}" ]]; then
+			local nodejsVersion="${NODEJS_VERSION:-18.16.0}"
 			echo "-----> Installing Node.js ${nodejsVersion}..."
 			install_nodejs "${nodejsVersion}" "${buildDir}/.heroku/nodejs" 2>&1 | sed -u 's/^/       /'
-			export PATH=${buildDir}/.heroku/nodejs/bin:$PATH
+			export PATH="${buildDir}/.heroku/nodejs/bin:${PATH}"
 		fi
 	fi
 }
 
-install_jdk() {
-	local install_dir=${1}
+function install_jdk() {
+	local install_dir="${1}"
 
 	((start = $(nowms)))
-	JVM_COMMON_BUILDPACK=${JVM_COMMON_BUILDPACK:-https://buildpack-registry.s3.us-east-1.amazonaws.com/buildpacks/heroku/jvm.tgz}
+	JVM_COMMON_BUILDPACK="${JVM_COMMON_BUILDPACK:-https://buildpack-registry.s3.us-east-1.amazonaws.com/buildpacks/heroku/jvm.tgz}"
 	mkdir -p /tmp/jvm-common
-	curl --fail --retry 3 --retry-connrefused --connect-timeout 5 --silent --location "$JVM_COMMON_BUILDPACK" | tar xzm -C /tmp/jvm-common --strip-components=1
+	curl --fail --retry 3 --retry-connrefused --connect-timeout 5 --silent --location "${JVM_COMMON_BUILDPACK}" | tar xzm -C /tmp/jvm-common --strip-components=1
 	# shellcheck source=/dev/null
 	source /tmp/jvm-common/bin/util
 	# shellcheck source=/dev/null
