afa1d3c23ff9a9ffe00f84b9ba2115fcec6752c6
Manuel Fuchs <manuel.fuchs@salesforce.com>
2025-11-26 19:19:27 +0100

    Overhaul integration tests with deterministic output matching

--- test/spec/fixtures/repos/clojurescript-example/Procfile
+++
@@ -1,1 +0,0 @@
-web: java $JVM_OPTS -cp target/my-app.jar clojure.main -m my-app.server
--- test/spec/fixtures/repos/lein-2-jdk-8/source/sample/core.clj
+++
@@ -1,5 +0,0 @@
-(ns sample.core
-  (:gen-class))
-
-(defn -main [& args]
-  (println "Welcome to my project! These are your args:" args))
