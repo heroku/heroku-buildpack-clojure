633443a12156b2320dbdcefa9b82115cdf22a4c4
Manuel Fuchs <manuel.fuchs@salesforce.com>
2025-12-11 14:09:04 +0100

    Add integration tests (#182)

--- test/spec/fixtures/repos/cljs-lein-npm-example/src/cljs/app/json.cljs
+++
@@ -1,6 +0,0 @@
-(ns app.json
-  (:require
-   [goog.net.XhrIo :as xhr]))
-
-(defn fetch-json [uri cb]
-  (xhr/send uri (fn [e] (-> e .-target .getResponseJson js->clj cb))))
---
+++ test/spec/fixtures/repos/non-clojure-app/README.md
@@ -0,0 +1,4 @@
+# Non-Clojure App
+
+This is a test fixture that does not contain a project.clj file.
+Used to test detection failure scenarios.
