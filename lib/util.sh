86d53dbfd258882cda4d06194305fec4be44a3d6
Manuel Fuchs <manuel.fuchs@salesforce.com>
2025-12-16 20:15:11 +0100

    Use shared util.sh (#195)

---
+++ lib/util.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+# This is technically redundant, since all consumers of this lib will have enabled these,
+# however, it helps Shellcheck realise the options under which these functions will run.
+set -euo pipefail
+
+# Exports configuration variables of a buildpacks ENV_DIR to environment variables.
+#
+# Only configuration variables which names pass the positive pattern and don't match the negative pattern
+# will be exported.
+#
+# Usage:
+# ```
+# util::export_env_dir "./env" "." "FORBIDDEN_ENV"
+# ```
+function util::export_env_dir() {
+	local env_dir="${1}"
+	local positive_pattern="${2:-"."}"
+	local negative_pattern="^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH${3:+"|"}${3})$"
+
+	for env_file in "${env_dir}"/*; do
+		if [[ -f "${env_file}" ]]; then
+			local env_name
+			env_name="$(basename "${env_file}")"
+
+			if [[ "${env_name}" =~ ${positive_pattern} ]] && ! [[ "${env_name}" =~ ${negative_pattern} ]]; then
+				export "${env_name}=$(cat "${env_file}")"
+			fi
+		fi
+	done
+}
+
+# Returns the current time in milliseconds since epoch.
+#
+# Usage:
+# ```
+# timestamp=$(util::nowms)
+# ```
+function util::nowms() {
+	date +%s%3N
+}
+
+# Copies a subdirectory from source to destination, replacing any existing content.
+#
+# Usage:
+# ```
+# util::cache_copy ".m2/wrapper" "${CACHE_DIR}" "${BUILD_DIR}"
+# ```
+function util::cache_copy() {
+	local subdirectory="${1}"
+	local source_dir="${2}"
+	local destination_dir="${3}"
+
+	local destination_path="${destination_dir}/${subdirectory}"
+	local source_path="${source_dir}/${subdirectory}"
+
+	rm -rf "${destination_path:?}"
+
+	if [[ -d "${source_path}" ]]; then
+		mkdir -p "${destination_path}"
+		cp -pr "${source_path}"/. "${destination_path}"
+	fi
+}
